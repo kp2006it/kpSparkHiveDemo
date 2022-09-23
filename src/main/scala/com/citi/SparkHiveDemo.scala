a65db5edd41c653be794ea63ecb398afc8ea0888
Krishna Pal <kp2006it@gmail.com>
2022-09-23 12:12:35 -0500

    Spark hive base code

---
+++ src/main/scala/com/citi/SparkHiveDemo.scala
@@ -0,0 +1,26 @@
+package com.citi
+
+import org.apache.spark.sql.SparkSession
+
+object SparkHiveDemo {
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+    //val conf = new SparkConf().setAppName(ScalaDemo.getClass.getName)
+    var a=3
+    var b=5
+    var c = a+b
+    println("a+b = "+c)
+
+    System.setProperty("hadoop.home.dir","C:\\winutils")
+    val spark:SparkSession = SparkSession.builder().master("local[1]")
+      .appName("ScalaDemo")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    println("spark-->"+spark)
+    val sampleSeq= Seq((1,"spark"),(2,"big data"))
+    val df = spark.createDataFrame(sampleSeq).toDF(colNames = "Course Id","Course Name")
+    df.show()
+    df.write.format(source = "csv").save(path = "samplesq")
+  }
+}
